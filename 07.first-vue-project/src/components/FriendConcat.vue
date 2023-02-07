26ee2408289d7e897d4aadd4a5e75db6a1cd1b5d
SallamRady <sallamrady@gmail.com>
2023-02-07 10:04:13 +0200

    Create my first project using vue cli

---
+++ 07.first-vue-project/src/components/FriendConcat.vue
@@ -0,0 +1,71 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      <span v-if="!showDetails">Show</span>
+      <span v-else>Hide</span>
+      Details
+    </button>
+    <ul v-if="showDetails">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDetails: false,
+      friend: {
+        id: "101",
+        name: "Ibrahim El-Morshady",
+        email: "ibrahim@test.com",
+        phone: "098 478 5619",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.showDetails = !this.showDetails;
+    },
+  },
+};
+</script>
+
+<style>
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
