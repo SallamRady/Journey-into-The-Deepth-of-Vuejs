1b1b8977b2101c5844b188d7da59529f2af03d7a
SallamRady <sallamrady@gmail.com>
2023-02-09 12:03:11 +0200

    Long Deeper Look into Vue Components

---
+++ 09.third-vue-project/src/components/CourseGoal.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <h3>Course Goals</h3>
+        <ul>
+            <li v-for="(goal,index) in goals" :key="goal">
+                <slot name="goalItem" :goal="goal" :index="index"></slot>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                goals:[
+                    'Finish the course and learn Vue!',
+                    'Became a Vue Master!'
+                ]
+            }
+        }
+    }
+</script>
---
+++ 09.third-vue-project/src/components/TheUser.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <BaseCard>
+      <template v-slot:cardHeader>
+        <h3>{{ fullName }}</h3>
+        <BaseBadge :type="role" :caption="role.toUpperCase()"></BaseBadge>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </BaseCard>
+  </section>
+</template>
+
+<script>
+import BaseBadge from "./BaseBadge.vue";
+import BaseCard from "./BaseCard.vue";
+
+export default {
+  components: { BaseBadge, BaseCard },
+  props: ["fullName", "infoText", "role"],
+};
+</script>
+
+<!-- <style scoped> -->
+<style>
+section {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+section div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
